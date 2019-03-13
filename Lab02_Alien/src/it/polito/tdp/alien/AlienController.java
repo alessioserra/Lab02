d610a751e12ecb4c9b0463c0d5853ee8e65869eb
zierp <zierp@DESKTOP-RURJMSA>
2019-03-13 11:08:50 +0100

    v1.0

--- Lab02_Alien/src/it/polito/tdp/alien/AlienController.java
+++ Lab02_Alien/src/it/polito/tdp/alien/AlienController.java
@@ -1,14 +1,7 @@
 package it.polito.tdp.alien;
 
-/**
- * Sample Skeleton for 'Alien.fxml' Controller Class
- */
-
-
-
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -16,40 +9,42 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class AlienController {
-	
+
     @FXML
     private ResourceBundle resources;
+
     @FXML
     private URL location;
+
     @FXML
-    private TextField txtWord;
+    private TextField txtInput;
+
     @FXML
-    private TextArea txtResult;
+    private Button handleTranslate;
+
     @FXML
-    private Button btnTranslate;
+    private TextArea txtMessaggi;
+
     @FXML
-    private Button btnReset;
-        
-    
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-    	assert txtWord != null : "fx:id=\"txtWord\" was not injected: check your FXML file 'Alien.fxml'.";
-    	assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Alien.fxml'.";
-    	assert btnTranslate != null : "fx:id=\"bntTranslate\" was not injected: check your FXML file 'Alien.fxml'.";
-    	assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Alien.fxml'.";
-    	
-    }
-  
-    
+    private Button handleClearText;
+
     @FXML
-    void doTranslate(ActionEvent event) {
-    	    	
-    }
-    
-    
-    @FXML
-    void doReset(ActionEvent event) {
+    void doClearText(ActionEvent event) {
 
     }
-    
+
+    @FXML
+    void doTranslate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert txtInput != null : "fx:id=\"txtInput\" was not injected: check your FXML file 'Alien.fxml'.";
+        assert handleTranslate != null : "fx:id=\"handleTranslate\" was not injected: check your FXML file 'Alien.fxml'.";
+        assert txtMessaggi != null : "fx:id=\"txtMessaggi\" was not injected: check your FXML file 'Alien.fxml'.";
+        assert handleClearText != null : "fx:id=\"handleClearText\" was not injected: check your FXML file 'Alien.fxml'.";
+
+    }
 }
+
